5aaf9ecd6d6893878efc2ca6db33e166c179d162
tayrapolikar <tayra.polikar@post.idc.ac.il>
2023-12-21 18:53:56 +0200

    TEST 5

--- LinearEq.java
+++ LinearEq.java
@@ -10,8 +10,15 @@ public class LinearEq {
         Double a = Double.parseDouble(args[0]);
         Double b = Double.parseDouble(args[1]);
         Double c = Double.parseDouble(args[2]);
-        Double result = (c-b) / a;
-        System.out.println(a +" * x + " + b + " = " + c);
-        System.out.println("x =" + result);
+
+        Double result = (c - b) / a;
+
+        System.out.println(a + " * x + " + b + " = " + c);
+
+        System.out.print("x = ");
+        if (result >= 0) {
+            System.out.print(" ");
+        }
+        System.out.println(result);
     }
 }
