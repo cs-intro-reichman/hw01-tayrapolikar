c3f7fa1cf72ca16bbbe0b979da4a08200fa3e71a
tayrapolikar <tayra.polikar@post.idc.ac.il>
2023-12-23 08:49:23 +0200

    test 8

--- LinearEq.java
+++ LinearEq.java
@@ -7,11 +7,11 @@
 public class LinearEq {
     // Put your code here
         public static void main(String[] args) {
-            Double a = Double.parseDouble(args[0]);
-            Double b = Double.parseDouble(args[1]);
-            Double c = Double.parseDouble(args[2]);
+            double a = Double.parseDouble(args[0]);
+            double b = Double.parseDouble(args[1]);
+            double c = Double.parseDouble(args[2]);
 
-            Double result = (c - b) / a;
+            double result = (c - b) / a;
 
             System.out.println(a + " * x + " + b + " = " + c);
 
